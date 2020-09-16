d05cab6dc9fd02869ae92e08ce22cebde387fd01
nvansturgill <vanburen.nia@gmail.com>
2020-09-16 14:10:35 -0400

    Create about.js for testing

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => (
+  <>
+    <h1>About the Author</h1>
+    <p>
+      Deserunt cupidatat consectetur et in incididunt. Esse anim voluptate
+      exercitation et exercitation eu est cupidatat Lorem incididunt sint
+      nostrud id anim. Labore cillum dolore nulla elit. Officia tempor officia
+      minim eu sunt esse esse pariatur eu. Ut consectetur aliqua minim ex
+      laborum officia adipisicing id eu.
+    </p>{' '}
+  </>
+);
