58d329964cd536983f045c87d901f86dccf9f609
nvansturgill <vanburen.nia@gmail.com>
2020-09-16 16:03:02 -0400

    Edit pages for client load time
    - Edit about and index pages to use gatby Link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default () => (
   <>
-    <h1>About the Author</h1>
+    <h1>About this Repository</h1>
     <p>
       Deserunt cupidatat consectetur et in incididunt. Esse anim voluptate
       exercitation et exercitation eu est cupidatat Lorem incididunt sint
       nostrud id anim. Labore cillum dolore nulla elit. Officia tempor officia
       minim eu sunt esse esse pariatur eu. Ut consectetur aliqua minim ex
       laborum officia adipisicing id eu.
-    </p>{' '}
+    </p>
+    <Link to="/">&larr; Return Home</Link>
   </>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => (
+  <>
+    <h1>Introduction to Gatsby</h1>
+    <p>
+      Culpa enim magna irure ut sit id officia in excepteur culpa est in.
+      Reprehenderit magna fugiat veniam magna ipsum cupidatat enim ipsum.
+      Laboris eiusmod irure eu nulla ullamco voluptate duis laboris laborum
+      velit tempor duis anim do. Culpa laborum velit cupidatat nulla commodo
+      laborum. Eu occaecat nulla sunt nostrud magna nulla laboris duis. Non
+      cupidatat minim anim ipsum tempor velit nulla. Occaecat esse cillum enim
+      aliqua non veniam magna veniam ullamco laborum excepteur nisi consequat.
+    </p>
+    <Link to="/about/">Learn More &rarr;</Link>
+  </>
+);
