aa269048431264f54aa6f370507c7ba7fe41abbb
nvansturgill <vanburen.nia@gmail.com>
2020-09-17 13:16:25 -0400

    Add gatsby-config file for metadata

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'An Experiment in Gatsby',
+    description: 'An experimental website build in the interest of learning ',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+};
