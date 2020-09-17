8d4c52ad5d3bd24096a3cebfb3b1256e17a097a9
nvansturgill <vanburen.nia@gmail.com>
2020-09-17 13:52:29 -0400

    Add site metadata using GraphQL
    Add metadata to site head using GraphQL query and React Helmet plugin
    - npm install 'react-helmet' and 'gatsby-plugin-react-helmet'
    - JSON packages updated from npm helmet installs
    - Add "metadata.js" containing query to ~/src/hooks
    - Edit layout.js file to insert metadata into Helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,27 +1,40 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import { Global, css } from '@emotion/core';
+import useSiteMetadata from '../hooks/metadata';
 
-const Layout = ({ children }) => (
-  <>
-    <Global
-      styles={css`
-        @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;1,400&family=Montserrat:ital,wght@0,400;0,500;1,400;1,500&display=swap');
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
 
-        body {
-          background-color: #fff;
-          color: #000;
-          font-size: 16px;
-          font-family: 'Montserrat', sans-serif;
-          margin: 1.4rem;
-          max-width: 90vw;
-        }
-        h1 {
-          font-family: 'Lora', serif;
-        }
-      `}
-    />
-    <main> {children} </main>{' '}
-  </>
-);
+  return (
+    <>
+      <Global
+        styles={css`
+          @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;1,400&family=Montserrat:ital,wght@0,400;0,500;1,400;1,500&display=swap');
+
+          body {
+            background-color: #fff;
+            color: #000;
+            font-size: 16px;
+            font-family: 'Montserrat', sans-serif;
+            margin: 1.4rem;
+            max-width: 90vw;
+          }
+          h1 {
+            font-family: 'Lora', serif;
+          }
+        `}
+      />
+
+      <Helmet>
+        <html lang="en-us" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+
+      <main> {children} </main>
+    </>
+  );
+};
 
 export default Layout;
